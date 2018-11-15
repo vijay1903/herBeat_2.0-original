c5b4f72a7b1c9cb2c94e2dcf22820934b1b32fb4
vijayvishwakarma <vijayv1903@gmail.com>
2018-11-14 19:41:26 -0500

    some change

--- database/db.js
+++ database/db.js
@@ -1,9 +1,10 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Pass@123',
-  database : 'herbeat'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database : process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 connection.connect(function(err){
     if(err){
